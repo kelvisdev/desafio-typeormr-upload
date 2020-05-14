db070c6c3494ff301f76099978ff990d5aedd7f7
Kelvis Borges <kelvisbcc@gmail.com>
2020-05-13 22:19:22 -0300

    Add Migration and registration of Transactions

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,56 @@
 // import AppError from '../errors/AppError';
 
+import { getCustomRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CategoriesRepository from '../repositories/CategoriesRepository';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getCustomRepository(CategoriesRepository);
+    let categoryEntity = new Category();
+
+    const findCategoryInSameTitle = await categoriesRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (findCategoryInSameTitle) {
+      categoryEntity.id = findCategoryInSameTitle.id;
+    } else {
+      categoryEntity = categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(categoryEntity);
+    }
+
+    const category_id = categoryEntity.id;
+    const transaction = transactionsRepository.create({
+      type,
+      title,
+      value,
+      category_id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
